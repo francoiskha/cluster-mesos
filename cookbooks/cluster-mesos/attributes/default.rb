a81f5aa8935fb768a95c6de56c7e59747e005e4f
francoiskha <francois.kha@gmail.com>
2015-05-20 21:21:29 +0200

    correcting issues and deleting cookbook wide 5 hour timeout

--- cookbooks/cluster-mesos/attributes/default.rb
+++ cookbooks/cluster-mesos/attributes/default.rb
@@ -1,3 +1,1 @@
 default['mesos'] = {}
-# docker timeout set to 5 hours 'cause, you know, slow internet connexion
-default['docker']['container_cmd_timeout'] = 18000
--- cookbooks/cluster-mesos/recipes/docker_registry.rb
+++ cookbooks/cluster-mesos/recipes/docker_registry.rb
@@ -6,32 +6,34 @@ docker_container 'docker-registry' do
   env 'SETTINGS_FLAVOR=local'
   volume '/mnt/docker:/docker-storage'
 end
-docker_registry 'http://localhost:5000'
-[{name: "boune/nginx", source: "/vagrant/docker/nginx-hello/Dockerfile"},
- {name: "boune/nginxtodo", source: "/vagrant/docker/nginx-todo/Dockerfile"},
- {name: "boune/todolist", source: "/vagrant/docker/todolist/Dockerfile"}].each do |image|
+
+images = [{name: "boune/nginx", source: "/vagrant/docker/nginx-hello/Dockerfile", action: :build_if_missing},
+ {name: "boune/nginxtodo", source: "/vagrant/docker/nginx-todo/Dockerfile", action: :build_if_missing},
+ {name: "boune/todolist", source: "/vagrant/docker/todolist/Dockerfile", action: :build_if_missing},
+ {name: "tutum/hello-world", action: :pull_if_missing},
+ {name: "mongo:3.0.2", action: :pull_if_missing}
+]
+images.each do |image|
+  docker_image image[:name] do
+    # tag "localhost:5000/#{image[:name]}:latest"
+    source image[:source] unless image[:source].nil?
+    action image[:action]
+    # timeout set to 5 hours 'cause, you know, slow internet connexion
+    cmd_timeout 18000
+  end
+end
+docker_registry 'http://localhost:5000/' do
+  username 'admin'
+  password 'docker'
+end
+images.each do |image|
   docker_image image[:name] do
     tag "localhost:5000/#{image[:name]}:latest"
-    source image[:source]
-    action :build_if_missing
-  end
-  docker_image "push #{image[:name]}" do
     action :push
+    #notifies :remove, "docker_image[#{image[:name]}]", :immediately
   end
-  # docker_image "push #{image[:name]}" do
-  #   action :remove
-  # end
-end
-
-%w(mongo:3.0.2 tutum/hello-world).each do |image_name|
-  docker_image image_name do
-    tag "localhost:5000/#{image_name}:latest"
-    action :pull_if_missing
-  end
-  docker_image "push #{image_name}" do
-    action :push
-  end
-  # docker_image "push #{image[:name]}" do
+  # if the notifies is not correct buts that's a chef resource with the same name
+  # docker_image "localhost:5000/#{image[:name]}:latest" do
   #   action :remove
   # end
 end
